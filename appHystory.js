9feb963e76809f59f6f86370c3c502ab40df35df
Bernavschi Natalia <natb@list.ru>
2023-04-10 13:28:55 +0300

    agaugat buton Materiale, TA, favorite la fiecare lectie, +/- la accordeon

--- appHystory.js
+++ appHystory.js
@@ -15,32 +15,40 @@ function initialization() {
      videoSourse[0].classList.remove("hide");
 }
 
-
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-
-
-
     const elements = document.querySelectorAll('.active');
     elements.forEach(element => {
       element.classList.remove('active');
     });
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    //  this.classList.remove("active");
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
         itemDescr.forEach((card) => {
-        card.style.display = "none";;
+        card.style.maxHeight = null;
         });
-      panel.style.display = "block";
-      this.classList.add("active");
-      videoSourse.forEach((card) => {
-        card.classList.add("hide");
-      });
-      videoSourse[this.dataset.num-1].classList.remove("hide");
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        this.classList.add("active");
+        videoSourse.forEach((card) => {
+          card.classList.add("hide");
+        });
+        videoSourse[this.dataset.num-1].classList.remove("hide");
     }
   });
 }
 
+const favoriteButtons = document.querySelectorAll('.add-bookmark-btn ');
+
+favoriteButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle("active");
+    if (button.classList.contains('active')) {
+      button.innerHTML = `<i class="fa-solid fa-bookmark"></i>`;
+    } else {
+      button.innerHTML = `<i class="fa-regular fa-bookmark"></i>`;;
+    }
+
+  });
+});
 
