f5ad757832960d6567892ce56e3195d4b4720558
Bernavschi Natalia <natb@list.ru>
2023-04-12 08:49:44 +0300

    Adaugat algoritmul de trecere pe subiectele videoului

--- appHystory.js
+++ appHystory.js
@@ -1,7 +1,10 @@
 let itemDescr = document.querySelectorAll(".panel")
 let videoSourse = document.querySelectorAll("#scrollToVideo iframe")
-var acc = document.getElementsByClassName("accordion");
-var i;
+const favoriteButtons = document.querySelectorAll('.add-bookmark-btn ');
+const acc = document.getElementsByClassName("accordion");
+let videoBreakpoints = document.querySelectorAll(".video-breakpoints-item");
+let i;
+let videoIndex = 0;
 
 initialization();
 function initialization() {
@@ -18,7 +21,7 @@ for (i = 0; i < acc.length; i++) {
     elements.forEach(element => {
       element.classList.remove('active');
     });
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
@@ -27,15 +30,18 @@ for (i = 0; i < acc.length; i++) {
         });
         panel.style.maxHeight = panel.scrollHeight + "px";
         this.classList.add("active");
+        let videoSourse = document.querySelectorAll("#scrollToVideo iframe")
         videoSourse.forEach((card) => {
           card.classList.add("hide");
         });
-        videoSourse[this.dataset.num-1].classList.remove("hide");
+        videoIndex = this.dataset.num-1;
+        videoSourse[videoIndex].classList.remove("hide");
+        
     }
   });
 }
 
-const favoriteButtons = document.querySelectorAll('.add-bookmark-btn ');
+
 
 favoriteButtons.forEach(button => {
   button.addEventListener('click', () => {
@@ -48,4 +54,25 @@ favoriteButtons.forEach(button => {
 
   });
 });
+
+videoBreakpoints.forEach(button => {
+  button.addEventListener('click', event => {
+   // videoSourse[videoIndex].outerHTML = '<iframe class="" width="100%" height="360" src="https://www.youtube.com/embed/yV9BAp0JzNA?start=48&autoplay=1&amp;enablejsapi=1" title="Conferinţa de Pace de la Paris" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen controls></iframe>';
+    videoSourse[videoIndex].outerHTML = '<iframe class="" width="100%" height="360" src="https://www.youtube.com/embed/yV9BAp0JzNA?start=149&autoplay=1&amp;enablejsapi=1" title="Conferinţa de Pace de la Paris" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen controls></iframe>';
+  
+    videoSourse = document.querySelectorAll("#scrollToVideo iframe")
+    videoSourse[videoIndex].addEventListener('load', () => {
+      const playerWindow = videoSourse[videoIndex].contentWindow;
+      setTimeout(function() {
+        playerWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+      }, 500);
+    });
+ 
+  })
+})
+
+
+
+
+
 
