4cb9cd8d36086dd401db3552f54b08f689ec7bcc
Bernavschi Natalia <natb@list.ru>
2023-04-19 19:19:22 +0300

    Pagina cu lista de teste la tema curenta am facut-o sa se construiasca dinamic

--- script/appListTest.js
+++ script/appListTest.js
@@ -1,11 +1,49 @@
 const linksToTest = document.querySelectorAll(".item a");
 let lectureNumber = parseInt(new URLSearchParams(window.location.search).get('lecture'));
-console.log(lectureNumber); 
+let lesson = document.querySelector(".container");
+// console.log(lectureNumber); 
 
-// linksToTest.forEach(link => {
-//     link.addEventListener("click", (e) => {
-//         e.target.href = e.target.href + '?lecture=${lectureNumber}';
-//         console.log(e.target.href)
-//     })
-// })
+async function app(){
+    try { 
+        const response = await fetch("../lectiiHystory.json");
+         if (!response.ok) { 
+            throw new Error(`HTTP error! status: ${response.status}`); } 
+            const data = await response.json();
+  
+            let template = document.querySelector("#containerTemplate").innerHTML;
+            lesson = document.querySelector(".container");
+    
+          let renderedHtml = Mustache.render(template, data[lectureNumber-1]);
+          lesson.innerHTML += renderedHtml;
+  
+          } 
+          catch (error) { 
+             console.error('Error:', error); 
+         }
+    
+    try { 
+        const response = await fetch("teste.json");
+         if (!response.ok) { 
+            throw new Error(`HTTP error! status: ${response.status}`); } 
+            const data = await response.json();
+            let testeCurente = data.filter(test => test.lesson == lectureNumber)
+
+            let template = document.querySelector("#listTemplate").innerHTML;
+            let testList = document.querySelector(".container .list");
+    
+            testeCurente.forEach(test => {
+              let renderedHtml = Mustache.render(template, test);
+              testList.innerHTML += renderedHtml;
+            });  
+          } 
+          catch (error) { 
+             console.error('Error:', error); 
+         }
+
+
+
+
+  
+  }
+  app();
 
